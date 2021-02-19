dbcf51def7e515dec72ee2b864bce31ecfe7642d
Jake Steck <60509027+jrsteck@users.noreply.github.com>
2021-02-18 20:30:59 -0500

    Add project files.

---
+++ FirstProgram/FirstProgram.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+
+int main()
+{
+
+    int favorite_number;
+
+    std::cout << "Enter your favorite number between 1 and 100: ";
+
+    std::cin >> favorite_number;
+
+    std::cout << "ME TOO! THAT IS MY FAVORITE NUMBER!" << std::endl;
+
+    return 0;
+}
+
